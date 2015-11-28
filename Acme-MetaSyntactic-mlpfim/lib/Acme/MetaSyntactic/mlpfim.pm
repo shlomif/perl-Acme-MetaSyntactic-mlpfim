42a989ac4f20948e5672b38d17929bb766bb20b2
Shlomi Fish <shlomif@shlomifish.org>
2015-11-28 15:52:56 +0200

    Got the tests to pass.

--- Acme-MetaSyntactic-mlpfim/lib/Acme/MetaSyntactic/mlpfim.pm
+++ Acme-MetaSyntactic-mlpfim/lib/Acme/MetaSyntactic/mlpfim.pm
@@ -3,5 +3,12 @@ package Acme::MetaSyntactic::mlpfim;
 use strict;
 use warnings;
 
+our $VERSION = '0.0.1';
+
 1;
 
+=head1 NAME
+
+Acme::MetaSyntactic::mlpfim - a meta syntactic variable for everypony!
+
+=cut
