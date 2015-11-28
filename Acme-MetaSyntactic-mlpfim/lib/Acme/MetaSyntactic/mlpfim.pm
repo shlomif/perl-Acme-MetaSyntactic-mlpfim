c4fd531e342bab96a338bb38f72eea7e5ea0047e
Shlomi Fish <shlomif@shlomifish.org>
2015-11-28 16:12:04 +0200

    Add names until the main antagonists.

--- Acme-MetaSyntactic-mlpfim/lib/Acme/MetaSyntactic/mlpfim.pm
+++ Acme-MetaSyntactic-mlpfim/lib/Acme/MetaSyntactic/mlpfim.pm
@@ -5,10 +5,80 @@ use warnings;
 
 our $VERSION = '0.0.1';
 
+use parent 'Acme::MetaSyntactic::List';
+
+__PACKAGE__->init();
+
 1;
 
 =head1 NAME
 
 Acme::MetaSyntactic::mlpfim - a meta syntactic variable for everypony!
 
+=head1 DESCRIPTION
+
+Acme::MetaSyntactic::mlpfim is a L<Acme::MetaSyntactic> theme which provides
+names of characters from the I<My Little Pony - Friendship is Magic> franchise.
+See L<http://mlp.wikia.com/wiki/Characters>.
+
+=head1 COPYRIGHTS AND TRADEMARKS INFORMATION
+
+As far as the originators of this CPAN distribution (= Shlomi Fish)
+is concerned, this distribution may be used under the terms of the MIT/X11
+licence. However, it contains data that may infringe on the copyrights and
+trademarks of the owners of the I<My Little Pony - Friendship is Magic>
+franchise. Note that all the names were collected from this page in the MLP-FiM
+Wikia wiki: L<http://mlp.wikia.com/wiki/Characters> .
+
 =cut
+
+__DATA__
+Apple_Bloom
+Apple_Rose
+Apple_Strudel
+Applejack
+Aunt_Orange
+Auntie_Applesauce
+Babs_Seed
+Big_McIntosh
+Diamond_Tiara
+Discword
+Featherweight
+Fluttershy
+Goldie_Delicious
+Granny_Smith
+Hayseed_Turnip_Truck
+King_Sombra
+Lord_Tirek
+Maud_Pie
+Mr_Cake
+Mrs_Cake
+Nightmare_Moon
+Peacy_Pie
+Pinkie_Pie
+Pipsqueak
+Pound_Cake
+Prince_Blueblood
+Princess_Amore
+Princess_Cadance
+Princess_Celestia
+Princess_Luna
+Pumpkin_Cake
+Queen_Chrysalis
+Rainbow_Dash
+Rarity
+Rumble
+Sctootaloo
+Shining_Armor
+Silver_Spoon
+Snais
+Snips
+Spike
+Starlight_Glimmer
+Sunny_Daze
+Sweetie_Belle
+Twilight_Sparkle
+Uncle_Orange
+Zipporwhill
+braeburn
+twist
